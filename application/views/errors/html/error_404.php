6d895c5ac0e87b0d781e7ebfbc24ec68b868981b
korneliussb <korneliussb@student.ce.undip.ac.id>
2020-04-19 20:27:47 +0700

    404

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -6,6 +6,103 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <html lang="en">
 
 <head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>Halaman tidak ditemukan</title>
+
+	<!-- Custom fonts for this template-->
+	<link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+	<!-- Custom styles for this template-->
+	<link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+	<!-- Page Wrapper -->
+	<div id="wrapper">
+
+
+
+		<!-- Content Wrapper -->
+		<div id="content-wrapper" class="d-flex flex-column">
+
+			<!-- Main Content -->
+			<div id="content">
+
+				<!-- Topbar -->
+				<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+
+
+
+
+				</nav>
+				<!-- End of Topbar -->
+
+				<!-- Begin Page Content -->
+				<div class="container-fluid">
+
+					<!-- 404 Error Text -->
+					<div class="text-center">
+						<div class="error mx-auto" data-text="404">404</div>
+						<p class="lead text-gray-800 mb-5">Halaman tidak ditemukan</p>
+						<p class="text-gray mb-0">Lebih baik anda kembali</p>
+						<a href="http://localhost/spkfu/"><i class="fas fa-arrow-left"></i>&nbsp; ke halaman utama</a>
+					</div>
+
+				</div>
+				<!-- /.container-fluid -->
+
+			</div>
+			<!-- End of Main Content -->
+
+			<!-- Footer -->
+			<footer class="sticky-footer bg-white">
+				<div class="container my-auto">
+					<div class="copyright text-center my-auto">
+						<span>Copyright &copy;<a href="https://github.com/korneliussb"> Kornelius Satria B</a> 2020</span>
+					</div>
+				</div>
+			</footer>
+			<!-- End of Footer -->
+
+		</div>
+		<!-- End of Content Wrapper -->
+
+	</div>
+	<!-- End of Page Wrapper -->
+
+
+
+
+	<!-- Bootstrap core JavaScript-->
+	<script src="assets/vendor/jquery/jquery.min.js"></script>
+	<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+	<!-- Core plugin JavaScript-->
+	<script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+	<!-- Custom scripts for all pages-->
+	<script src="assets/js/sb-admin-2.min.js"></script>
+
+	<!-- <?= config_item('base_url') . 'assets'; ?> -->
+</body>
+
+</html>
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+
+<head>
 	<meta charset="utf-8">
 	<title>404 Page Not Found</title>
 	<style type="text/css">
@@ -72,4 +169,4 @@ defined('BASEPATH') or exit('No direct script access allowed');
 	</div>
 </body>
 
-</html>
+</html> -->
