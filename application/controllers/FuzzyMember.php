4b98c6a0d52c0c7f40949445271c4dd745ca7993
korneliussb <korneliussb@student.ce.undip.ac.id>
2020-05-12 09:25:30 +0700

    fix Controller name

--- application/controllers/FuzzyMember.php
+++ application/controllers/FuzzyMember.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class FuzzyMember extends CI_Controller
+class Fuzzymember extends CI_Controller
 {
     // public function __construct()
     // {
